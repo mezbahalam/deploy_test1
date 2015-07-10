76b3aef32446da2cc6f27da491560eed034a5816
mezbahalam <mezbahalam26@.com>
2015-07-10 22:34:46 +0000

    deploy set

---
+++ config/unicorn.rb
@@ -0,0 +1,7 @@
+working_directory File.expand_path("../..", __FILE__)
+worker_processes 2
+listen "/tmp/unicorn.sock" 
+timeout 30
+pid "/tmp/unicorn.pid"
+# stdout_path "/vagrant/log/unicorn.log"
+# stderr_path "/vagrant/log/unicorn.log"
