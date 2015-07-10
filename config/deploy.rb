c152f39934b72347392f001e1332fb24ef725f08
mezbahalam <mezbahalam26@.com>
2015-07-10 23:32:58 +0000

    add forword agent

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,6 +3,7 @@ lock '3.4.0'
 
 set :application, 'deploy'
 set :repo_url, 'https://github.com/mezbahalam/deploy_test1'
+set :user, "vagrant"
 set :deploy_to, "/home/vagrant/apps/deploy"
 set :use_sudo, false
 # Default branch is :master
@@ -13,7 +14,7 @@ set :use_sudo, false
 
 # Default value for :scm is :git
 set :scm, :git
-
+set :ssh_options, {forward_agent: true}
 # Default value for :format is :pretty
 # set :format, :pretty
 
@@ -37,13 +38,21 @@ set :scm, :git
 
 namespace :deploy do
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
+  task :start do
+  	sudo "service nginx start"
+  	sudo "service postgresql start"
+  	run "cd #{current_path} && bundle exec unicorn -c config/unicorn.rb -E production -D"
   end
+ 
+  
+
+  # after :restart, :clear_cache do
+  #   on roles(:web), in: :groups, limit: 3, wait: 10 do
+  #     # Here we can do anything such as:
+  #     # within release_path do
+  #     #   execute :rake, 'cache:clear'
+  #     # end
+  #   end
+  # end
 
 end
