4fa77052ec84e9a4283fa5492e9dbb4cab8a4b57
mezbahalam <mezbahalam26@gmail.com>
2015-07-11 13:12:59 +0600

    unicorn change

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,7 @@
-working_directory File.expand_path("../..", __FILE__)
+working_directory  "/vagrant"#File.expand_path("../..", __FILE__)
 worker_processes 2
-listen "/tmp/unicorn.sock" 
+listen "/tmp/unicorn.deploy.sock" 
 timeout 30
-pid "/tmp/unicorn.pid"
+pid "/vagrant/tmp/pids/unicorn.pid"
 # stdout_path "/vagrant/log/unicorn.log"
 # stderr_path "/vagrant/log/unicorn.log"
